16ce6e373a1c98d17a0d564d1fe3d0e45f479d3e
Karim Barth <karim.barth@me.com>
2019-03-13 12:23:37 +0100

    use voxelfilter

--- cartographer/mapping/internal/2d/local_trajectory_builder_2d.cc
+++ cartographer/mapping/internal/2d/local_trajectory_builder_2d.cc
@@ -56,16 +56,18 @@ LocalTrajectoryBuilder2D::TransformToGravityAlignedFrameAndFilter(
       sensor::CropRangeData(sensor::TransformRangeData(
                                 range_data, transform_to_gravity_aligned_frame),
                             options_.min_z(), options_.max_z());
-  /*
+
   return sensor::RangeData{
       cropped.origin,
       sensor::VoxelFilter(options_.voxel_filter_size()).Filter(cropped.returns),
       sensor::VoxelFilter(options_.voxel_filter_size()).Filter(cropped.misses)};
-  */
+
+  /*
   return sensor::RangeData{
     cropped.origin,
     sensor::RandomFilter(options_.adaptive_voxel_filter_options()).Filter(cropped.returns),
     sensor::RandomFilter(options_.adaptive_voxel_filter_options()).Filter(cropped.misses)};
+  */
  }
 
 std::unique_ptr<transform::Rigid2d> LocalTrajectoryBuilder2D::ScanMatch(
@@ -230,15 +232,16 @@ LocalTrajectoryBuilder2D::AddAccumulatedRangeData(
       extrapolator_->ExtrapolatePose(time);
   const transform::Rigid2d pose_prediction = transform::Project2D(
       non_gravity_aligned_pose_prediction * gravity_alignment.inverse());
-  /*
+  
   const sensor::PointCloud& filtered_gravity_aligned_point_cloud =
       sensor::AdaptiveVoxelFilter(options_.adaptive_voxel_filter_options())
           .Filter(gravity_aligned_range_data.returns);
-  */
+
+  /*
   const sensor::PointCloud& filtered_gravity_aligned_point_cloud =
     sensor::RandomFilter(options_.adaptive_voxel_filter_options())
       .Filter(gravity_aligned_range_data.returns);
-
+  */
   if (filtered_gravity_aligned_point_cloud.empty()) {
     return nullptr;
   }
