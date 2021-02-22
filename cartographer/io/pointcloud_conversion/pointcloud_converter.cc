38aaa00c837c3ceee6acc0f5a0d3091594219348
Kevin Daun <daun@sim.tu-darmstadt.de>
2021-02-22 16:40:17 +0100

    Add comment with sample TSDF generation.

--- cartographer/io/pointcloud_conversion/pointcloud_converter.cc
+++ cartographer/io/pointcloud_conversion/pointcloud_converter.cc
@@ -5,6 +5,12 @@
 #include "cartographer/transform/transform.h"
 #include "gflags/gflags.h"
 #include "glog/logging.h"
+#include "mapping/3d/hybrid_grid_tsdf.h"
+#include "cartographer/mapping/3d/range_data_inserter_3d.h"
+#include "cartographer/mapping/3d/tsdf_range_data_inserter_3d.h"
+
+#include "cartographer/common/lua_parameter_dictionary.h"
+#include "cartographer/common/lua_parameter_dictionary_test_helpers.h"
 
 #ifdef WITH_OPEN3D
 #include "open3d/Open3D.h"
@@ -21,6 +27,44 @@ namespace mapping {
         std::make_shared<open3d::geometry::PointCloud>();
     open3d::io::ReadPointCloud(point_cloud_filename, *cloud,{"auto", true, true, true});
     open3d::visualization::DrawGeometries({cloud});
+
+//    Sample TSDF Generation in Cartographer --> see evaluation/scan_matching_evaluation.cc
+//    cartographer::mapping::proto::RangeDataInserterOptions3D
+//        range_data_inserter_options;
+//    auto parameter_dictionary_range_data_inserter = common::MakeDictionary(R"text(
+//        return {
+//      range_data_inserter_type = "PROBABILITY_GRID_INSERTER_3D",
+//      probability_grid_range_data_inserter = {
+//        hit_probability = 0.55,
+//        miss_probability = 0.49,
+//        num_free_space_voxels = 2,
+//      },
+//      tsdf_range_data_inserter = {
+//        relative_truncation_distance = 4,
+//        maximum_weight = 1000.,
+//        num_free_space_voxels = 0,
+//        project_sdf_distance_to_scan_normal = true,
+//        weight_function_epsilon = 1,
+//        weight_function_sigma = 4.,
+//        normal_estimate_max_nn = 20.,
+//        normal_estimate_radius = 0.3,
+//      },})text");
+//    range_data_inserter_options =
+//        cartographer::mapping::CreateRangeDataInserterOptions3D(
+//            parameter_dictionary_range_data_inserter.get());
+//
+//    mapping::ValueConversionTables conversion_tables_;
+//    mapping::HybridGridTSDF hybrid_grid_tsdf(
+//        0.05,
+//        range_data_inserter_options.tsdf_range_data_inserter_options_3d()
+//            .relative_truncation_distance(),
+//        range_data_inserter_options.tsdf_range_data_inserter_options_3d()
+//            .maximum_weight(),
+//        &conversion_tables_);
+//    mapping::TSDFRangeDataInserter3D tsdf_range_data_inserter(
+//        range_data_inserter_options);
+//    tsdf_range_data_inserter.Insert(sample.range_data, &hybrid_grid_tsdf);
+
   }
 }  // namespace mapping
 }  // namespace cartographer
