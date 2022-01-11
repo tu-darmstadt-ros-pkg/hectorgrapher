615ec8433ee5462c28ae2bd5a2590e8106a8ac73
Leonard Hampel <leonard.hampel@stud.tu-darmstadt.de>
2022-01-12 00:29:26 +0100

    Updated lua-configuration-file to generate a cubic point cloud

--- cartographer/io/pointcloud_conversion/configurations/cubic.lua
+++ cartographer/io/pointcloud_conversion/configurations/cubic.lua
@@ -4,18 +4,21 @@ options = {
     generateCubicPointcloud = true,
     sidelengthCubicPointcloud = 0.1,
     distancePointsCubicPointcloud = 0.004,
-    noiseCubicPointcloud = 0.0,
+    noiseCubicPointcloud = 0.003,
 
     uniformDownSample = false,
-
+    voxelDownSample = false,
     removeRadiusOutliers = false,
 
     cutRoofZAxis = false,
 
     normalOrientationNearestNeighbours = 40,
 
-    absoluteVoxelSize = 0.005,
-    absoluteTruncationDistance = 0.01,
+    absoluteHighResVoxelSize = 0.1,
+    absoluteLowResVoxelSize = 0.4,
+    relativeHighResTruncationDistance = 2.5,
+    relativeLowResTruncationDistance = 2.5,
+
     maxTSDFWeight = 10.0,
 
 
